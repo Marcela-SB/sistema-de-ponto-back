e3c848b69a83c30d7b4478458ecff07ef43e3c55
Marcela-SB <marcela013sb@hotmail.com>
2026-01-26 12:59:03 -0300

    Primeiro commit

---
+++ src/main/java/com/deart/sistema_de_ponto_back/SistemaDePontoBackApplication.java
@@ -0,0 +1,13 @@
+package com.deart.sistema_de_ponto_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePontoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePontoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deart/sistema_de_ponto_back/SistemaDePontoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deart.sistema_de_ponto_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePontoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
