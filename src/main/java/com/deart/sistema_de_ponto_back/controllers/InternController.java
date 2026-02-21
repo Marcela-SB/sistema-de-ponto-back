2d748e98ffdaf75cc7e65af13b7a447954e1631c
Marcela-SB <marcela013sb@hotmail.com>
2026-02-21 07:52:51 -0300

    refactor(intern): padroniza endpoint de atualização e limpa imports
    - Altera a rota de update de `/interns/update/{externalId}` para `/interns/{externalId}`.
    - Remove import não utilizado.

--- src/main/java/com/deart/sistema_de_ponto_back/controllers/InternController.java
+++ src/main/java/com/deart/sistema_de_ponto_back/controllers/InternController.java
@@ -15,13 +15,10 @@ import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-
 
 
 @RestController
@@ -66,7 +63,7 @@ public class InternController {
     }
     
     // verificar ROLE (apenas ADMIN ou SUPERV. responsável)
-    @PutMapping("/update/{externalId}")
+    @PutMapping("/{externalId}")
     public ResponseEntity<InternResponse> updateIntern(@PathVariable UUID externalId, @RequestBody InternUpdateRequest updateRequest) {
         Intern entity = service.update(externalId, updateRequest);
         InternResponse response = mapper.toResponse(entity);
